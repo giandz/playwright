f64cde43fe162cda67f1907be70c31412cc38221
Gianfranco Díaz <50000548+giandz@users.noreply.github.com>
2024-08-23 17:25:39 -0400

    Wrong test file

--- moshitest.spec.ts
+++ moshitest.spec.ts
@@ -1,18 +1,34 @@
-import { test } from '@playwright/test';
+import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+test('Moshi thing', async ({ page }) => {
+	
+	await page.goto('https://www.airalo.com');
+	
+	const box = page.getByTestId('search-input');
+	await box.click();
+	await box.pressSequentially("Japan");
+	
+	const japan = page.getByTestId('Japan-name');
+	await japan.click();
+	
+	const buybutton = page.getByRole('link', { name: 'Moshi Moshi Moshi Moshi  COVERAGE Japan  DATA 1 GB  VALIDITY 7 Days PRICE $4' }).getByRole('button');
+	await buybutton.click();
+	
+	const operator = page.getByTestId('sim-detail-operator-title');
+	await expect(operator).toContainText('Moshi Moshi');
+		
+	const cardData = page.getByTestId('sim-detail-info-list');
+	
+	const coverage = cardData.getByTestId('COVERAGE-value');
+	await expect(coverage).toContainText('Japan');
+	
+	const data = cardData.getByTestId('DATA-value');
+	await expect(data).toContainText('1 GB');
+	
+	const validity = cardData.getByTestId('VALIDITY-value');
+	await expect(validity).toContainText('7 Days');
+	
+	const price = cardData.getByTestId('PRICE-value');
+	await expect(price).toContainText('$4.50');
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
